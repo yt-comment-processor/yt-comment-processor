c2d182b48826745968b75b17478a4f6db56e0ebc
netcore2939 <netcore29390@gmail.com>
2022-06-28 14:26:41 +0900

    setup.py 버그 수정

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ requiredpacakges = ['tensorflow']
 if platform.system() == 'Linux':
     print('리눅스 환경 감지됨')
     print('/usr/bin/bash 서브프로세스를 실행하는 중')
-    for i in requiredpacakges:
-        subprocess.run(['/usr/bin/bash', 'pip --install ' + requiredpacakges[i]])
+    for i in range(len(requiredpacakges)):
+        subprocess.run(['/usr/bin/bash', 'pip install ' + requiredpacakges[i]])
 elif platform.system() == 'Windows':
     print('윈도우 환경 감지됨')
     print('powershell.exe 서브프로세스를 실행하는 중')
-    for i in requiredpacakges:
-        subprocess.run(['powershell.exe', 'py -m pip --install ' + requiredpacakges[i]])
+    for i in range(len(requiredpacakges)):
+        subprocess.run(['powershell.exe', 'py -m pip install ' + requiredpacakges[i]])
